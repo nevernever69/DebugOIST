77b763075bc3a44404ef9c5fd0228dc5c22fc291
devashish <mshubh612@gmail.com>
2025-03-17 01:40:03 +0530

    done with fotter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SpotlightNewDemo } from "@/components/HomePage";
 import { LogoCarouselDemo } from "@/components/Carousel";
+import { Footer } from "@/components/FotterPage";
 
 export default function LandingPage() {
     return (
       <div>
         <SpotlightNewDemo />
         <LogoCarouselDemo />
+        <Footer />
       </div>
     );
   }
