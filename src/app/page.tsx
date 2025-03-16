18a92656c18cfb46a1ee63e3f8c98e07ff4ad62a
devashish <mshubh612@gmail.com>
2025-03-17 01:34:48 +0530

    done with carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { SpotlightNewDemo } from "@/components/HomePage";
+import { LogoCarouselDemo } from "@/components/Carousel";
 
 export default function LandingPage() {
     return (
       <div>
         <SpotlightNewDemo />
+        <LogoCarouselDemo />
       </div>
     );
   }
