55810eb7706ac75a97e55d3df2b993d461324356
devashish <mshubh612@gmail.com>
2025-03-17 02:15:42 +0530

    done with glowing card

---
+++ components/GlowingCardPage.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import * as React from "react"
+import { GridBackground } from "./ui/glowing-card";
+
+
+export function GridBackgroundDemo() {
+  return (
+      <GridBackground
+        className="w-full"
+        title='Welcome to JatinVerse'
+        description="Transforming your ideas into stunning visual experiences. Explore the world of creative design with us."
+      />
+  )
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SpotlightNewDemo } from "@/components/HomePage";
 import { LogoCarouselDemo } from "@/components/Carousel";
 import { Footer } from "@/components/FotterPage";
+import { GridBackgroundDemo } from "@/components/GlowingCardPage";
 
 export default function LandingPage() {
     return (
       <div>
         <SpotlightNewDemo />
+        <GridBackgroundDemo />
         <LogoCarouselDemo />
         <Footer />
       </div>
